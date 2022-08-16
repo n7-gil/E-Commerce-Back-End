8d971d09ca1f045bddb4e96ae26d79f028fa5ac7
N7 GIL <gilbertoaescobedo@gmail.com>
2022-08-15 20:03:13 -0700

    moved all files inside develop folder. deleted develop folder. set up mysql connection. set up intitial route to connect to server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const mysql = require("mysql2");
 // import sequelize connection
+const sequelize = require("sequelize");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ecommerce_db",
+  },
+  console.log(`Connected to the ecommerce_db.`)
+);
 
 app.use(routes);
 
